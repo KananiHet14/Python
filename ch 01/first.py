031ee6e1fc1bc127cd9463940edb342780d7e432
Kanani Het <tech.kananihet@gmail.com>
2026-04-29 16:12:44 +0530

    little changes

--- ch 01/first.py
+++ ch 01/first.py
@@ -2,3 +2,20 @@ print("Hello World!")
 # python filename.py is used to run the code in terminal
 # the output of program is  what the program shows after running the code.
 
+# modules
+#  a python module is like  a tool box filled with special tools (CODES)
+
+
+# how to install module?
+# pip install module_name
+# pip is a package manager for python that allows you to install and manage additional libraries and dependencies that are not included in the standard library.
+
+
+# what is REPL?
+# A Python REPL lets you run code line by line and see results instatly.
+
+
+# how to do comments ?
+# by uding ctrl + / or by using # symbol before the line of code you want to comment out.
+# it is used to explain the code and also for the code tthat we dont want to run but want to keep it in the code for future reference.
+# by using comments we hide code or execute but dont want to show it in the output.
