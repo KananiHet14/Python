662aa510d90bd13f62b8a7c7cc6b14fe3b274629
Kanani Het <tech.kananihet@gmail.com>
2026-04-30 12:53:33 +0530

    a small code example done

---
+++ ch 02/variables.py
@@ -0,0 +1,18 @@
+# a variable is the name given to a memory location in a program which store a values.
+
+# for example : a = 10
+
+# keywords are reserved words in python that have a special meaning and cannot be used as variable names or identifiers.
+
+# identifiers are the names given to variables, functions, classes, and other objects in Python. They must follow certain rules and conventions.
+
+# data types are the classification of data items. It represents the kind of value that a variable can hold.
+
+# integers = whole numbers , floats = decimal numbers , strings = sequence of characters , booleans = True or False , none = represents the absence of a value.
+
+# for example : 
+
+a = 10
+b = 3.14
+c = a+b;
+print(c)
