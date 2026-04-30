0decd96ac9a2a3c3ff630ffc20c00f5a89feb3ed
Kanani Het <tech.kananihet@gmail.com>
2026-04-30 13:22:47 +0530

    datatype done

--- ch 02/datatypes.py
+++ ch 02/datatypes.py
@@ -3,3 +3,9 @@ b = 3.14; #this is a float
 c = "Hello, World!"; #this is a string
 d = True; #this is a boolean
 e = None; #this represents the absence of a value
+
+# rules for variable names:
+# vriable name starts from letter only or underscore after you use number in it.
+# no space allowed in variable name
+# variable name should not be a keyword
+# variable name should be meaningful and descriptive
