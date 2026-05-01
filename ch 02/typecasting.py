49b71ca0c47a88a0e49672f484995311c5f56f93
Kanani Het <tech.kananihet@gmail.com>
2026-05-01 13:36:49 +0530

    type casting done

---
+++ ch 02/typecasting.py
@@ -0,0 +1,10 @@
+# typecasting is the process of converting one data type to another data type
+# there are two types of typecasting
+# 1. implicit typecasting
+# 2. explicit typecasting
+# implicit typecasting is done by the python interpreter automatically
+# explicit typecasting is done by the programmer using the built-in functions
+
+a = 10
+b = float(a) # explicit typecasting
+print(type(a) , type(b))
