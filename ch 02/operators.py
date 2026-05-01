6352b39e1783f58b873bedf9422d1035f5800e67
Kanani Het <tech.kananihet@gmail.com>
2026-05-01 13:31:33 +0530

    operator done

---
+++ ch 02/operators.py
@@ -0,0 +1,23 @@
+# arithmetic operators
+# + , - , * , / , % , ** , //
+
+a = 10
+b = 2
+print(a+b , a-b , a*b , a/b , a%b , a**b , a//b)
+
+# assignment operators
+# = , += , -= , *= , /= , %= , **= , //=
+a += 5 
+a -= 3
+a *= 2
+a /= 4
+a %= 3
+a **= 2
+a //= 2
+print(a)
+
+# comparation operators
+# == , != , > , < , >= , <=
+
+# logical operators
+# and , or , not
