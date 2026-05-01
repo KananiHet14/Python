2ab8d5e3d656dcec012372abaf070bcdbd10142d
Kanani Het <tech.kananihet@gmail.com>
2026-05-01 13:44:24 +0530

    Input output done

---
+++ ch 02/IO.py
@@ -0,0 +1,11 @@
+# for take input from user we use input() function
+name = input("Enter your name: ")
+print("Hello " + name)
+
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+print("number 1 is " + a)
+print("number 2 is " + b)
+
+print(float(a) + float(b)) # typecasting string to float
