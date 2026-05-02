124eb68fafc549d28b3dd1fb038b4ad1b056586c
Kanani Het <tech.kananihet@gmail.com>
2026-05-02 13:40:27 +0530

    negative indexing

--- ch 03/strings.py
+++ ch 03/strings.py
@@ -8,7 +8,7 @@
 
 # a string in python can be sliced for getting a part of the string.
 # strings are immutable in python, which means that we cannot change the value of a string after it is created.
-# the indexing of string starts from 0 to (length-1) in python or by the last character with -1 to -(lenngth) in python.
+# the indexing of string starts from 0 to (length-1) in python.
 
 # syntax = sl = name[ind_start:ind_end]
 
@@ -16,3 +16,6 @@ name = "kanani het"
 print(len(name)) # length of the string
 name_sliced = name[0:6]
 print(name_sliced) # slicing the string from index 0 to 5
+
+
+# negrative indexing : negative indexing can also used as shown in figure above. -1 corresponds to the (length - 1) index , -2 to (length - 2).
