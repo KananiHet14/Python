e08094293c840744ad32158b70b97731fa575f86
Kanani Het <tech.kananihet@gmail.com>
2026-05-02 13:12:27 +0530

    string slicing little part done

---
+++ ch 03/strings.py
@@ -0,0 +1,18 @@
+# String = string is a sequence of characters.
+
+# String can be defined in three ways:
+# 1. Using single quotes: 'Hello, World!'
+# 2. Using double quotes: "Hello, World!"
+# 3. Using triple quotes: '''Hello, World!''' or """Hello, World
+
+
+# a string in python can be sliced for getting a part of the string.
+# strings are immutable in python, which means that we cannot change the value of a string after it is created.
+# the indexing of string starts from 0 to (length-1) in python or by the last character with -1 to -(lenngth) in python.
+
+# syntax = sl = name[ind_start:ind_end]
+
+name = "kanani het"
+print(len(name)) # length of the string
+name_sliced = name[0:6]
+print(name_sliced) # slicing the string from index 0 to 5
