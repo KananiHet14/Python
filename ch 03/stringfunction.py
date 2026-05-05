53f4c6c4c5b8fbcc1aa8568944b70b801bb5019d
Kanani Het <tech.kananihet@gmail.com>
2026-05-05 12:20:54 +0530

    string functions

--- ch 03/stringfunction.py
+++ ch 03/stringfunction.py
@@ -1,19 +1,27 @@
 # len() = This function return the length of the string
 
-name = "KANANI"
+name = "kanani"
 print(len(name))
 
 print("-----------------------------")
 
 # String.endswith() = this funciton tells wether the variable string ends with the string specified part or not.
                     #   if the string ends with the specified parth then it returns true otherwise it returns false.
-print(name.endswith("I"))
-print(name.endswith("N"))
+print(name.endswith("i"))
+print(name.endswith("n"))
 
 print("-----------------------------")
 
 # String.startswith() = this function tells wether the variable string starts with the string specified part or not.
                     #   if the string starts with the specified parth then it returns true otherwise it returns false.
 
-print(name.startswith("K"))
-print(name.startswith("A"))
+print(name.startswith("k"))
+print(name.startswith("a"))
+
+print("-----------------------------")
+
+# capitalize() = this function converts the first character of string to uppercase.
+print(name.capitalize())
+
+
+print("-----------------------------")
