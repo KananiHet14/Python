ca8ddcbdfd5f98c8346a521a91a6d4f7d3442e8f
Kanani Het <tech.kananihet@gmail.com>
2026-05-05 12:18:23 +0530

    len , ends and starts with function doen

--- ch 03/stringfunction.py
+++ ch 03/stringfunction.py
@@ -3,7 +3,17 @@
 name = "KANANI"
 print(len(name))
 
+print("-----------------------------")
+
 # String.endswith() = this funciton tells wether the variable string ends with the string specified part or not.
                     #   if the string ends with the specified parth then it returns true otherwise it returns false.
 print(name.endswith("I"))
 print(name.endswith("N"))
+
+print("-----------------------------")
+
+# String.startswith() = this function tells wether the variable string starts with the string specified part or not.
+                    #   if the string starts with the specified parth then it returns true otherwise it returns false.
+
+print(name.startswith("K"))
+print(name.startswith("A"))
