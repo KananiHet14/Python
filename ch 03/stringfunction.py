58b3a8b3c7cdbf1c24a848ed3e0fabe244e01c7f
Kanani Het <tech.kananihet@gmail.com>
2026-05-05 12:15:14 +0530

    learned strings 2 functions"

---
+++ ch 03/stringfunction.py
@@ -0,0 +1,8 @@
+# len() = This function return the length of the string
+
+name = "KANANI"
+print(len(name))
+
+# String.endswith() = this funciton tells wether the variable string ends with the string specified part or not.
+                    #   if the string ends with the specified parth then it returns true otherwise it returns false.
+print(name.endswith("I"))
