ecc938980bb3179fb3152762ad3543e58263b599
Kanani Het <tech.kananihet@gmail.com>
2026-06-05 14:06:26 +0530

    solved one most basic problem

---
+++ Problems/CH04.py
@@ -0,0 +1,22 @@
+# Question 1 = write a program to store marks in lists
+
+marks = []
+
+marks1 = int(input("Enter marks : "))
+marks.append(marks1) 
+marks2 = int(input("Enter marks : "))
+marks.append(marks2)
+marks3 = int(input("Enter marks : "))
+marks.append(marks3)
+marks4 = int(input("Enter marks : "))
+marks.append(marks4)
+marks5 = int(input("Enter marks : "))
+marks.append(marks5)
+marks6 = int(input("Enter marks : "))
+marks.append(marks6)
+mark = int(input("Enter marks : "))
+marks.append(mark)
+
+
+marks.sort()
+print(marks)
