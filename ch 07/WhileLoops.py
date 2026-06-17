9fc5f9f2b46e6d7a9a1d42114f95cb8fa89e6d88
Kanani Het <tech.kananihet@gmail.com>
2026-06-17 13:29:31 +0530

    while loop and list loop done

--- ch 07/WhileLoops.py
+++ ch 07/WhileLoops.py
@@ -1,5 +1,12 @@
-i = 1
+# i = 1
 
-while(i<=50):
-    print(i)
-    i += 1
+# while(i<=50):
+#     print(i)
+#     i += 1
+
+# list traversal (list using while loop)
+# my_list = [1, 2, 3, 4, 5]
+# i = 0
+# while(i < len(my_list)):
+#     print(my_list[i])
+#     i += 1
