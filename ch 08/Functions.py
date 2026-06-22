37148cb3622255dda472c7464c72ff60c6d9032f
Kanani Het <tech.kananihet@gmail.com>
2026-06-22 13:52:02 +0530

    Functions done

--- ch 08/Functions.py
+++ ch 08/Functions.py
@@ -1,5 +1,5 @@
 # function ia a group of statements performing a specific task.
-
+'''
 def avg() :
     a = int(input("Enter a number : "))
     b = int(input("Enter a number : "))
@@ -9,5 +9,27 @@ def avg() :
 
 
 avg()
+'''
 
 # types of functions : 1] built in 2] user defined
+
+# parameters and arguments
+def goodDay(name):
+    print("Good Day " + name)
+
+goodDay("Kanani")
+
+# return statement
+def add(a,b):
+    return a+b
+
+result = add(5, 10)
+print(result)
+
+# default parameters
+def goodDay(name, ending="Thank you!"):
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodDay("Kanani")
+# if you provide a diff vale then its considered a diff value
