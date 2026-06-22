dcf21d2aa7535274636e18467bdf91fa57f61ad2
Kanani Het <tech.kananihet@gmail.com>
2026-06-22 14:46:21 +0530

    some problems solved

--- ch 08/ps08.py
+++ ch 08/ps08.py
@@ -1,3 +1,4 @@
+"""
 def greatest(a,b,c):
     if (a > b and a > c):
         return a
@@ -11,3 +12,30 @@ b = int(input("enter second number : "))
 c = int(input("enter third number : "))
 
 print(f"The greatest number is : {greatest(a,b,c)}")
+
+
+# f to c
+def f_to_c(f):
+    return 5*(f-32)/9
+
+f = int(input("Enter temperature in F: "))
+print(f_to_c(f))
+
+
+#sum of first n natural numbers
+def sum(n):
+    if(n==1):
+        return 1
+    return sum(n-1) + n
+print(sum(4))
+
+# pattern function
+def pattern(n):
+    if(n==0):
+        return
+    print("*"*n)
+    pattern(n-1)
+
+pattern(5)
+"""
+
