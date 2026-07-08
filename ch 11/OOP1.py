357aae8fb757b6fb12b9f1360c6981ae8c3697bb
Kanani Het <tech.kananihet@gmail.com>
2026-07-08 12:01:04 +0530

    oop class done

---
+++ ch 11/OOP1.py
@@ -0,0 +1,14 @@
+'''
+oop = solving a problem by using object or class is one of the most popular approach in programing.
+      this is called as object oriented programming (OOP).
+'''
+
+'''
+class = class is the blueprint of the object. it is a collection of objects.
+'''
+
+class Student:
+    language = "python"
+    salary = 10000
+
+print(Student.language," ",Student.salary)
