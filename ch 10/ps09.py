1b55c0171ea2d20a458ed443ae2275afe9bb2831
Kanani Het <tech.kananihet@gmail.com>
2026-07-08 12:43:47 +0530

    little changes

--- ch 10/ps09.py
+++ ch 10/ps09.py
@@ -9,27 +9,3 @@ if("het" in content):
 else:
     print("het is not available")
 '''
-
-
-#  question 2 
-import random
-
-def game():
-    print("you are playing the game...")
-    score = random.randint(1, 62)
-    # fethch the highscore 
-    with open("highscore.txt") as f:
-        highscore = f.read()
-        if(highscore != ""):
-            highscore = int(highscore)
-        else:
-            highscore = 0
-    print(f"your score is : {score}")
-    if(score > highscore):
-        # write this score to the file
-        with open("highscore.txt" , "w") as f:
-            f.write(highscore)
-
-    return score
-
-game()
