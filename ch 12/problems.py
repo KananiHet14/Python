89352df6b6052dc3913c926aac0e26fd997606be
Kanani Het <tech.kananihet@gmail.com>
2026-07-14 12:00:02 +0530

    2 problems solved

---
+++ ch 12/problems.py
@@ -0,0 +1,37 @@
+"""class TwoDVector:
+    def __init__(self , i , j):
+        self.i = i
+        self.j = j
+    
+    def show(self):
+        print(f"2d vector is {self.i}i + {self.j}j")
+
+class ThreeDVEctor(TwoDVector):
+    def __init__(self , i , j , k):
+        super().__init__(i , j)
+        self.k = k
+
+    def show(self):
+        print(f"3d vector is {self.i}i + {self.j}j + {self.k}k")
+
+a = TwoDVector(1,2)
+a.show()
+b = ThreeDVEctor(1,2,3)
+b.show()"""
+
+"""
+class Animals:
+    pass
+
+class Pets(Animals):
+    pass
+
+class Dogs(Pets):
+    
+    @staticmethod
+    def bark():
+        print("Bow Bow!")
+
+d = Dogs()
+d.bark()
+"""
