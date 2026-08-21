8d68af5514c98b35433c293dee8e7a392b37bdf9
Kanani Het <tech.kananihet@gmail.com>
2026-08-21 14:55:11 +0530

    print stuffs

---
+++ Python-Basics/FunVariable.py
@@ -0,0 +1,19 @@
+"""
+# DATE = 21-08-2026
+
+# bug = a mistake that you make while writing a program
+# single = is a assignment operator its used to hand a value to a variable
+# Pseudocode is a step-by-step description of an algorithm written in simple English using a code-like structure.
+# print(*object) = means print take any object and print it on the screen
+# print(sep=' ') = sep means saperator , sep used to saperate the objecct with space or any other character.
+# print(end='\n') = \n means new line ,  end used to print the next object in new line.
+
+"""
+
+name = input("enter you name : ")
+print("hello " + name + " !")
+
+print("hello, ", end="\n")
+print(name)
+
+print("hello, " , name , sep="\n")
