0e2dd9392dcf7d0e32a633dcce1c9d52f0bea3ee
Kanani Het <tech.kananihet@gmail.com>
2026-08-22 09:32:15 +0530

    string and print related work done and started operators and int

--- Python-Basics/FunVariable.py
+++ Python-Basics/FunVariable.py
@@ -1,6 +1,4 @@
 """
-# DATE = 21-08-2026
-
 # bug = a mistake that you make while writing a program
 # single = is a assignment operator its used to hand a value to a variable
 # Pseudocode is a step-by-step description of an algorithm written in simple English using a code-like structure.
@@ -8,13 +6,31 @@
 # print(sep=' ') = sep means saperator , sep used to saperate the objecct with space or any other character.
 # print(end='\n') = \n means new line ,  end used to print the next object in new line.
 # \\ = the words between the backslash used to escaping
-# print(f"hello {name} !") = f string is a string 
+# print(f"hello {name} !") = f string is a string that contain varibale value + string also. there f means formate.
+# string.strip() = its a method that is used to remove the white-spaces from the string.
+# string.capitalize() = its a method that is used to capitalize the first letter of the string.
+# string.title() = its a methoda that is used to capitlized first letter of each word in the string.
+# string.upper() = its a method that is used to convert the string to uppercase.
+# string.lower() = its a method that is used to convert the string to lowercase.
+# string.split() = its a method that is used to split the string into a list of substrings.
 """
 
-name = input("enter you name : ")
-print("hello " + name + " !")
+# name = input("enter you name : ")
 
-print("hello, ", end="\n")
-print(name)
+# Printing the name using different methods
+# print("hello " + name + " !")
+# print("hello, ", end="\n")
+# print(name)
+# print("hello, " , name , sep="\n")
 
-print("hello, " , name , sep="\n")
+
+# String Methoda
+# print(name.strip())
+# print(name.capitalize())
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+# print(name.split("-"))
+
+
+# Operators
