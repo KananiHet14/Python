27ec4c13ed57faf5cd23e6cc68ab843e28e9c578
Kanani Het <tech.kananihet@gmail.com>
2026-09-06 11:32:23 +0530

    while loop and range function done

---
+++ Python-Basics/Loops.py
@@ -0,0 +1,17 @@
+"""
+while loop = if you have fixed values or you want to run the loop until the condition is true then you can use while loop.
+range function = if you want to run the loop for a fixed number of times then you can use range function with for loop.
+
+"""
+
+# # while loop
+# i = 0
+# while i != 3:
+#     print("het")
+#     i += 1
+
+
+# # forloop range function
+# for i in range(1 , 4):
+#     print(i)
+#     i += 1
