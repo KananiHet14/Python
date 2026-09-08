a57b3c1f921ee744376a6f1a41913278215522d7
Kanani Het <tech.kananihet@gmail.com>
2026-09-08 09:08:02 +0530

    iteration on list

--- Python-Basics/Loops.py
+++ Python-Basics/Loops.py
@@ -4,7 +4,14 @@ while true is used for infinite loop and you can break the loop using break stat
 range function = if you want to run the loop for a fixed number of times then you can use range function with for loop.
 for loop = if you want to run the loop in the specific range or do iteration then use for loop with range function.
 continue statemenr = if you want to skip the current iteration and move to the next iteration then you can use continue statement.
-break statement = if you want to break the loop and exit from the loop then you can use break statement.    
+break statement = if you want to break the loop and exit from the loop then you can use break statement. 
+iteration = it means repeating the process or executing a block of code typically to step throug element of a collectionone by one.
+len = len is a python inbuilt function that is used for check the length of the data.
+list = it is a data type that contain different typed of data. it is resizable and chanagable.
+dictionary = it is a key value pair that store data in the key and value. it is changable.
+set = store unique values but remove duplicates automatically.
+tuple = tuple is like a list but little catch is there is it cant be changable.
+
 """
 
 # # while loop
@@ -35,3 +42,15 @@ break statement = if you want to break the loop and exit from the loop then you 
 #     else:
 #         print("please enter a positive number !")
 #         continue
+
+
+# iteration on LIST
+# students = ["het" , "navya"]
+# for student in students:
+#     print(student)
+
+# students = ["het" , "navya"]
+# for student in range(len(students)):
+#     print(student + 1  , " "  , students[student])
+
+# iteration on DICT
