52ce1588b25fabc259c0902b64b3573d8ea67364
Kanani Het <tech.kananihet@gmail.com>
2026-09-14 11:34:34 +0530

    learned random choice function

---
+++ Python-Basics/Libraries.py
@@ -0,0 +1,19 @@
+"""
+module = its a library that have a one or more functions to built in. its used for reusability of code.
+random = python come with random library you will get modules and library when oyu install python first by default.
+import = it is a keywords that allows you ot import functions from library.
+from = it is a keywords that used to import function to specific library from specific function.
+random.choise() = used to take a random choise from  porvided data
+"""
+
+import random # or u can use from random import choice
+
+# your_choice = input("Enter heads or tails : ").strip().lower()
+# coin = random.choice(["heads" , "tails"])
+
+# if(your_choice == coin):
+#     print("you win")
+# elif(your_choice != coin):
+#     print("you lose")
+# else:
+#     print("please enter a valid inputs")
