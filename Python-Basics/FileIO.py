5f713916ef9e5eaea3135cfa021b17fc07c13854
Kanani Het <tech.kananihet@gmail.com>
2026-09-26 09:45:35 +0530

    a , with..as , r mode

--- Python-Basics/FileIO.py
+++ Python-Basics/FileIO.py
@@ -4,9 +4,10 @@ open =  in Python is a built-in function used to open files on your computer, re
 .write() = its a function that do help to write in file
 .close() = its a functions that used to close file and save the changes
 'a' mode = (Append mode) inside the open() function allows you to add new data to the end of an existing file without erasing its current content.
+with.....as = statement acts as a context manager that automatically handles opening and safely closing a file.
 """
 
-name = input("enter your name  :  ")
+# name = input("enter your name  :  ")
 
 # "w" mode
 # file = open("FileIO.txt" , "w")
@@ -15,6 +16,18 @@ name = input("enter your name  :  ")
 
 
 # "a" mode
-file = open("FileIO.txt" , "a")
-file.write(f"{name}\n")
-file.close()
+# file = open("FileIO.txt" , "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with....as
+# with open("FileIo.txt" , "a") as file:
+#     file.write(f"{name}\n")
+
+
+# "r" mode
+with open("FileIO.txt" , "r") as file:
+    lines = file.readlines()
+
+for line in lines:
+    print(line.strip()) # also you can use rstrip() but its returns a copy
