1c20c0405199fc542b7ef6739e27b01deaac7182
Kanani Het <tech.kananihet@gmail.com>
2026-09-26 10:41:46 +0530

    r mode and list sorted function

--- Python-Basics/FileIO.py
+++ Python-Basics/FileIO.py
@@ -5,6 +5,7 @@ open =  in Python is a built-in function used to open files on your computer, re
 .close() = its a functions that used to close file and save the changes
 'a' mode = (Append mode) inside the open() function allows you to add new data to the end of an existing file without erasing its current content.
 with.....as = statement acts as a context manager that automatically handles opening and safely closing a file.
+.sorted() = this is a list function that used to sort list
 """
 
 # name = input("enter your name  :  ")
@@ -26,8 +27,16 @@ with.....as = statement acts as a context manager that automatically handles ope
 
 
 # "r" mode
-with open("FileIO.txt" , "r") as file:
-    lines = file.readlines()
+# with open("FileIO.txt" , "r") as file:
+#     lines = file.readlines()
 
-for line in lines:
-    print(line.strip()) # also you can use rstrip() but its returns a copy
+# for line in lines:
+#     print(line.strip()) # also you can use rstrip() but its returns a copy
+
+# sorted function in list
+# names = []
+# with open("FileIO.txt") as file:
+#     for line in file:
+#         names.append(line.rstrip())
+# for name in sorted(names):
+#     print(f"hello , {name}")
