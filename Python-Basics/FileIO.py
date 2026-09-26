cf36d25d71b1c39605c4fd3e95838655cd4cfe20
Kanani Het <tech.kananihet@gmail.com>
2026-09-26 08:27:23 +0530

    opne , write and close

--- Python-Basics/FileIO.py
+++ Python-Basics/FileIO.py
@@ -1,12 +1,12 @@
 """"
 open =  in Python is a built-in function used to open files on your computer, returning a file object 
-'w' mode = allows you to open a file exclusively for writing
+'w' mode = allows you to open a file exclusively for writing if code run again its not keep old data[not append].
 .write() = its a function that do help to write in file
 .close() = its a functions that used to close file and save the changes
 """
 
 name = input("enter your name  :  ")
 
-file = open("names.txt" , "w")
+file = open("FileIO.txt" , "w")
 file.write(name)
 file.close()
